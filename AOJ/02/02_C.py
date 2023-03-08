0c4c60782aff47932c997186cf14ec4f1e691493
yuki-kito <kitosuke0223@gmail.com>
2023-03-08 22:57:07 +0900

    sortに書き直しました

--- AOJ/02/02_C.py
+++ AOJ/02/02_C.py
@@ -1,13 +1,7 @@
 x = list(map(int,input().split()))
-for j in range(2):
-    for i in range(1 , 3 - j ):
-        if x[ j + i ] < x[ j ]:
-            y = x[:]
-            x[ j ] = y[ j  +  i ]
-            x[ j + i ] = y[ j ]
 
-for a,b in enumerate(x,1):
+for a,b in enumerate(sorted(x),1):
     if a == 3:
         print(b)
     else:
-        print(b, end=' ')
+        print(b,end=' ')
