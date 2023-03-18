57f9d3aa21a2dd3d6971b78c3fc5cd4d471686ab
yuki-kito <kitosuke0223@gmail.com>
2023-03-18 10:01:10 +0900

    修正を行いました。

--- AOJ/07/07_B.py
+++ AOJ/07/07_B.py
@@ -8,14 +8,8 @@ while True:
 
 for (n1,x1) in array:
     count = 0
-    if n1 < 3 or x1 < 6:
-        print(0)
-        continue
     for i in range(1,n1 - 1) :
         for j in range(i + 1, n1):
             if (j < ((x1 - i) - j)) and (((x1 - i) - j) <= n1):
                 count += 1         
     print(count)
-
-
-
